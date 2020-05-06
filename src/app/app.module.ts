be73d16865672a8c09361a3a98307f4df2ffa102
Miguel Angel <miguelangelteletor@yahoo.es>
2020-05-06 20:46:39 +0200

    add the sidenav successfully,-mright now i´m work to add the buttons and more things

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SidenavComponent } from './components/sidenav/sidenav/sidenav.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +16,8 @@ import { SidenavComponent } from './components/sidenav/sidenav/sidenav.component
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
