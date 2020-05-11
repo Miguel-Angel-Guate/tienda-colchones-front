ff98ffe79aef1330a51ad1181bd7695591ebbc2b
Miguel Angel <miguelangelteletor@yahoo.es>
2020-05-11 11:24:08 +0200

    the login is working, the img is working on the screen-mfor anything check this commit

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -10,10 +10,10 @@ export class UserService {
   public user:User;
   constructor(private httpClient:HttpClient) {}
   login(credentials:object):Observable<any>{
-    return this.httpClient.post(environment.FRONT_URL+'/users/login',credentials);
+    return this.httpClient.post(environment.API_URL+'/users/login',credentials);
   }
   register(user:User):Observable<any>{
-    return this.httpClient.post(environment.FRONT_URL+'/users/register',user);
+    return this.httpClient.post(environment.API_URL+'/users/register',user);
   }
   setUser(user:User):void{
     this.user=user;
